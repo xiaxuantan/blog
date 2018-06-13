8510dbb76ac3bdde217e3adcd5bfc6f30ff6b16e
xiaxuantan <xx.peter.tan@gmail.com>
2018-06-13 16:58:32 +0800

    add new packages and adjust to react development mode

--- src/main/java/com/xuan24/blog/controller/PageController.java
+++ src/main/java/com/xuan24/blog/controller/PageController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class PageController {
 
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/space/*")
+    public String space() {
+        return index();
     }
 
 }
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import UserSpace from './user-space';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import UserSpace from './user-space'
+
+
+const Home = () => (
+    <h1>Hello World!</h1>
+);
+
+const App = () => (
+    <div>
+        <Content/>
+    </div>
+);
+
+const Content = () => (
+    <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/space/:name' component={UserSpace}/>
+    </Switch>
+);
+
 
 ReactDOM.render(
-    <UserSpace/>,
-    document.getElementById('user')
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
