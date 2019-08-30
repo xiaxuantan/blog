02906e6a81d7c2604d133592bcd783be5c12d7cc
xiaxuantan <xx.peter.tan@gmail.com>
2019-08-29 21:26:40 -0700

    update header and footer

--- src/main/js/footer.js
+++ src/main/js/footer.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
 
+import {faGithub, faLinkedin, faWeibo} from '@fortawesome/free-brands-svg-icons'
+import {Nav, Row} from "react-bootstrap";
+
+
+library.add(faGithub, faLinkedin, faWeibo);
 
 const Footer = () => (
     <div>
-        <hr/>
-        <div className="row">
+        <Row class="justify-content-center">
             <div className="col text-center">
                 <p>Copyright &#169; 2019 Xiaxuan Tan</p>
             </div>
-        </div>
+        </Row>
+
+        <Nav className="justify-content-center" activeKey="/home">
+            <Nav.Item>
+                <Nav.Link href="https://www.linkedin.com/in/xiaxuan-tan">
+                    <FontAwesomeIcon icon={faLinkedin} size="lg"/>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="https://www.github.com/xiaxuantan">f
+                    <FontAwesomeIcon icon={faGithub} size="lg"/>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="https://www.weibo.com/peterxuan">
+                    <FontAwesomeIcon icon={faWeibo} size="lg"/>
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+
     </div>
 );
 
