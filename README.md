1e1870ff2b7e9572ce95bd163fb15950fdb784f0
xiaxuantan <xx.peter.tan@gmail.com>
2019-08-31 16:47:58 -0700

    add documents

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # blog
+
+### Tech stack
+
+- Backend stack: JavaWeb (SpringBoot, SpringMVC, Spring, MyBatis)
+- Frontend stack: ReactJS, Redux, Webpack
+- Storage: AWS S3, Mysql
+
+### References:
+
+- [Integrate front end building tools with maven](https://github.com/eirslett/frontend-maven-plugin)
--- src/main/js/website.js
+++ src/main/js/website.js
@@ -6,11 +6,17 @@ import Col from "react-bootstrap/Col";
 
 const website = `
 
-# Website
+# blog
+
+### Tech stack
 
 - Backend stack: JavaWeb (SpringBoot, SpringMVC, Spring, MyBatis)
 - Frontend stack: ReactJS, Redux, Webpack
 - Storage: AWS S3, Mysql
+
+### References:
+
+- [Integrate front end building tools with maven](https://github.com/eirslett/frontend-maven-plugin)
 
 `;
 
